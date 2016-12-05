65fc9ee6517b6712b9289e14ff56d6509d449d45
Nafeu Nasir <nafeu.nasir@gmail.com>
2016-12-05 18:00:35 -0500

    Refactored dom factory pattern

--- public/js/components/dom-factory.js
+++ public/js/components/dom-factory.js
@@ -1,9 +1,8 @@
 $(function(){
 console.log("Loading: dom-factory");
 
-function d(type, id, attrs) {
+function d(type, attrs) {
   var out = $("<"+type+">");
-  if (id) out.attr("id", id);
   if (attrs) out.attr(attrs);
   return out;
 }
@@ -11,25 +10,18 @@ function d(type, id, attrs) {
 domFactory = {
   build: {
     chatMessage: function(data) {
-      var name = $("<span></span>")
-        .addClass("chat-name")
+      var name = d('span', { class: 'chat-name' })
         .css("color", getUsernameColor(data.user.name))
         .text(data.user.name);
-      var message = $("<span></span>")
-        .addClass("chat-message-text")
+      var message = d('span', { class: 'chat-message-text' })
         .text(data.message);
-      return $("<p></p>")
-        .addClass("chat-message")
-        .append(name)
-        .append(": ")
-        .append(message);
+      return d('p', { class: 'chat-message' })
+        .append(name, ": ", message);
     },
     updateMessage: function(data) {
-      var update = $("<span></span>")
-        .addClass("chat-update-text")
+      var update = d('span', { class: 'chat-update-text' })
         .text(data.text);
-      return $("<p></p>")
-        .addClass("chat-update")
+      return d('p', { class: 'chat-update' })
         .append(update);
     },
     startingDisplay: function(data) {
@@ -43,24 +35,30 @@ domFactory = {
     },
     questionDisplay: function(data) {
       return domFactory.assets.gameBoard()
-        .append(domFactory.assets.question(data))
-        .append(domFactory.assets.answers(data))
-        .append(domFactory.assets.players(data));
+        .append(
+          domFactory.assets.question(data),
+          domFactory.assets.answers(data),
+          domFactory.assets.players(data)
+        );
     },
     resultDisplay: function(data) {
       return domFactory.assets.gameBoard()
-        .append(domFactory.assets.topAnswer(data))
-        .append(domFactory.assets.scores(data))
-        .append(domFactory.assets.countdown(function(){
-          if (data.gameLength === 0)
-            return "showing winner in ";
-          return "next question in ";
-        }, 7));
+        .append(
+          domFactory.assets.topAnswer(data),
+          domFactory.assets.scores(data),
+          domFactory.assets.countdown(function(){
+            if (data.gameLength === 0)
+              return "showing winner in ";
+            return "next question in ";
+          }, 7)
+        );
     },
     endingDisplay: function(data) {
       return domFactory.assets.gameBoard()
-        .append(domFactory.assets.winner(data))
-        .append(domFactory.assets.nextBtn("Play again!"));
+        .append(
+          domFactory.assets.winner(data),
+          domFactory.assets.nextBtn("Play again!")
+        );
     },
     disconnectDisplay: function() {
       return domFactory.assets.gameBoard()
@@ -72,123 +70,113 @@ domFactory = {
   },
   assets: {
     gameBoard: function() {
-      return d('div', 'game-board');
+      return d('div', { id: 'game-board' });
     },
     hostLobby: function() {
-      return d('div', 'host-lobby')
+      return d('div', { id: 'host-lobby' })
         .append(
           domFactory.assets.nextBtn('Start Game.')
         )
       ;
     },
     participantLobby: function() {
-      return d('div', 'participant-lobby')
+      return d('div', { id: 'participant-lobby' })
         .append(
           d('h2').text("Waiting for host to start...")
         )
       ;
     },
     question: function(data) {
-      return d('div', 'display-question')
+      return d('div', { id: 'display-question' })
         .append(
-          d('div')
-            .addClass("display-question-asker")
+          d('div', { class: 'display-question-asker' })
             .text(function(){
               if (data.currQuestion.by.length > 0)
                 return data.currQuestion.by + " asks";
               else
                 return "anonymous asks:";
-            })
-        )
-        .append(
-          d('div')
-            .addClass("display-question-text")
+            }),
+          d('div', { class: 'display-question-text' })
             .text(data.currQuestion.q)
         );
     },
     answers: function(data) {
-      var out = d('div', 'display-answers');
+      var out = d('div', { id: 'display-answers' });
       var choices = data.currQuestion.a;
-      for (var i = 0; i < choices.length; i++) {
-        var choice = d('div')
-          .addClass("display-choice")
-          .text(choices[i])
-          .attr("onclick", "if (user.active) socket.emit('submitAnswer', { 'answer': " + i + "});");
+      choices.forEach(function(answer, index){
+        var choice = d('div', { class: 'display-choice' })
+          .text(answer)
+          .attr("onclick", "if (user.active) socket.emit('submitAnswer', { 'answer': " + index + "});");
         out.append(choice);
-      }
+      });
       return out;
     },
     players: function(data) {
-      var out = d('div', 'display-players');
+      var out = d('div', { id: 'display-players' });
       var playerIds = Object.keys(data.players);
-      for (var i = 0; i < playerIds.length; i++) {
-        var player = d('div', "userId-"+data.players[playerIds[i]].name)
-          .addClass("display-player")
-          .css("border-color", getUsernameColor(data.players[playerIds[i]].name));
-        var playerName = d('div')
-          .addClass("display-player-name")
-          .text(data.players[playerIds[i]].name)
-          .css("color", getUsernameColor(data.players[playerIds[i]].name));
-        var playerScore = d('div')
-          .addClass("display-player-score")
-          .text(data.players[playerIds[i]].score);
-        player
-          .append(playerName)
-          .append(playerScore);
+      playerIds.forEach(function(playerId){
+        var player = d('div', {
+            id: "userId-"+data.players[playerId].name,
+            class: "display-player",
+          })
+          .css("border-color", getUsernameColor(data.players[playerId].name));
+        var playerName = d('div', { class: 'display-player-name' })
+          .text(data.players[playerId].name)
+          .css("color", getUsernameColor(data.players[playerId].name));
+        var playerScore = d('div', { class: 'display-player-score' })
+          .text(data.players[playerId].score);
+        player.append(playerName, playerScore);
         out.append(player);
-      }
+      });
       return out;
     },
     scores: function(data) {
-      var out = d('div', "display-scores");
+      var out = d('div', { id: "display-scores" });
       var playerIds = Object.keys(data.players);
-      for (var i = 0; i < playerIds.length; i++) {
-        var player = d('div', "userId-"+data.players[playerIds[i]].name)
-          .addClass("display-player")
-          .css("border-color", getResultColor(data.players[playerIds[i]]))
-          .append(
-            d('div')
-              .addClass("display-player-name")
-              .text(data.players[playerIds[i]].name)
-              .css("color", getResultColor(data.players[playerIds[i]])))
-          .append(
-            d('div')
-              .addClass("display-player-increment")
-              .text(data.players[playerIds[i]].increment));
+      playerIds.forEach(function(playerId){
+        var player = d('div', {
+          id: "userId-"+data.players[playerId].name,
+          class: "display-player"
+        })
+        .css("border-color", getResultColor(data.players[playerId]))
+        .append(
+          d('div', { class: 'display-player-name' })
+            .text(data.players[playerId].name)
+            .css("color", getResultColor(data.players[playerId])),
+          d('div', { class: 'display-player-increment' })
+            .text(data.players[playerId].increment)
+        );
         out.append(player);
-      }
+      });
       return out;
     },
     topAnswer: function(data) {
-      var out = d('div', "display-popular");
+      var out = d('div', { id: 'display-popular' });
       if (data.tiedScoreCounter > 1) {
         out.append(
-          d('div')
-            .addClass("display-popular-header")
+          d('div', { class: 'display-popular-header' })
             .text("Even split. No points! No majority!"));
       } else {
-        var answer = d('div')
-          .addClass("display-popular-top")
+        var answer = d('div', { class: 'display-popular-top' })
           .text(data.topAnswer);
         out.append(
-          d('div')
-            .addClass("display-popular-header")
+          d('div', { class: 'display-popular-header' })
             .text("The most popular answer was")
         .append(answer));
       }
       return out;
     },
     winner: function(data) {
-      var out = d('div', 'display-winner');
+      var out = d('div', { id: 'display-winner' });
       var playerIds = Object.keys(data.players);
       var winner = "";
       var highestScore = 0;
-      for (var i = 0; i < playerIds.length; i++) {
-        if (data.players[playerIds[i]].score > highestScore) {
-          highestScore = data.players[playerIds[i]].score;
-          winner = data.players[playerIds[i]].name;
+      playerIds.forEach(function(playerId){
+        if (data.players[playerId].score > highestScore) {
+          highestScore = data.players[playerId].score;
+          winner = data.players[playerId].name;
         }
-      }
+      });
       out.text("The winner is " + winner + " with a score of " + highestScore);
       if (highestScore === 0) {
         out.text("Ugh, it was a tie... Lame.");
@@ -206,14 +194,11 @@ domFactory = {
       });
     },
     countdown: function(msg, seconds) {
-      return d('div', "countdown-timer")
+      return d('div', { id: "countdown-timer" })
         .append(
-          d('span')
-            .text(msg)
-            .addClass("countdown-timer-msg")
-        )
-        .append(
-          d('span', "countdown-timer-num")
+          d('span', { class: 'countdown-timer-msg' })
+            .text(msg),
+          d('span', { id: "countdown-timer-num" })
             .text(seconds)
         );
     },
