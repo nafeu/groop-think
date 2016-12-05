a418b60b1bc46028c7cd281f1ae97b5bb887578e
Nafeu Nasir <nafeu.nasir@gmail.com>
2016-12-05 15:25:33 -0500

    Split up scss file into partials

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ var browserSync = require('browser-sync');
 var nodemon = require('gulp-nodemon');
 var sass = require('gulp-sass');
 
-gulp.task('default', ['browser-sync'], function () {
+gulp.task('default', ['sass', 'browser-sync'], function () {
 });
 
 gulp.task('sass', function(){
-  return gulp.src("public/scss/*.scss")
+  return gulp.src("public/scss/**/*.scss")
     .pipe(sass())
     .pipe(gulp.dest('public/css'));
 });
@@ -15,11 +15,11 @@ gulp.task('sass', function(){
 gulp.task('browser-sync', ['nodemon'], function() {
   browserSync.init(null, {
     proxy: "http://localhost:8000",
-      files: ["public/**/*.*", "!public/scss/*.scss", "!server.js", "!components/*.js"],
+      files: ["public/**/*.*", "!public/scss/**/*.scss", "!server.js", "!components/*.js"],
       browser: "google chrome",
       port: 7000,
   });
-  gulp.watch("app/scss/*.scss", ['sass']);
+  gulp.watch("public/scss/**/*.scss", ['sass']);
 });
 
 gulp.task('nodemon', function (cb) {
@@ -29,7 +29,7 @@ gulp.task('nodemon', function (cb) {
   return nodemon({
     script: 'server.js',
     ext: 'js css scss',
-    ignore: ['public/scss/styles.scss', 'public/css/styles.css'],
+    ignore: ['public/scss/**/*.scss', 'public/css/styles.css'],
     env: { 'DEBUG' : 'true' }
   }).on('start', function () {
     // to avoid nodemon being started multiple times
